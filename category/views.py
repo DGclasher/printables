a40fb54ae19cf221393703a21c7f4f71adcac29a
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-02-26 10:32:18 +0530

    dev

--- category/views.py
+++ category/views.py
@@ -1,6 +1,7 @@
 from .models import *
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
 @login_required()
@@ -8,18 +9,20 @@ def add_to_cart(request, pk):
     products = Product.objects.get(pk=pk)
     ncart = Cart.objects.create(user=request.user, products=products)
     ncart.save()
-    return redirect('/')
+    return HttpResponseRedirect(reverse("products"))
 
 @login_required()
 def remove_from_cart(request, pk):
     products = Product.objects.get(pk=pk)
     ncart = Cart.objects.get(products=products)
     ncart.delete()
-    return redirect('/')
+    return HttpResponseRedirect(request.path_info)
 
 @login_required()
 def cart(request):
     cart_items = Cart.objects.filter(user=request.user)
-
-    return render(request, 'cart.html', {"cart_items" : cart_items})
+    total_price = 0
+    for cart_item in cart_items:
+        total_price += cart_item.products.price
+    return render(request, 'cart.html', {"cart_items" : cart_items, "total_price" : total_price})
     pass
